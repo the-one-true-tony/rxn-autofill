313d04b1253dc64b74749b607b26a10b99897595
Tony Wang <sspy45@gmail.com>
2017-08-07 19:10:07 -0700

    added access-control headers

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ client.on("error", (err) => {
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api', (req, res) => {
   let query = req.query.name;   // query
   let maxReturn = 10;           // set number of results returned
