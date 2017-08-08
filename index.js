d5c7e924eb927cd8d5cfb2b4334fed34f19e1017
Tony Wang <sspy45@gmail.com>
2017-08-08 11:37:40 -0700

    final commit

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
 app.get('/api', (req, res) => {
   let query = req.query.name;   // query
   let maxReturn = 10;           // set number of results returned
